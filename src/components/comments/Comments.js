5e030dbec75d0ff45168abb3ea735ade49c054a8
Michal Labucki <labucki.michal@gmail.com>
2022-01-18 21:18:03 +0100

    added an comment options will be fixed

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,15 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from '../comments/CommentsList'
 
 const Comments = () => {
+
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const {bikerouteId} = params;
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(bikerouteId)
+  }, [bikerouteId, sendRequest]);
+
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  let comments;
+  const addedCommentHandler = useCallback(() => {
+   sendRequest(bikerouteId);
+  },[sendRequest, bikerouteId]);
+
+  if(status === 'pending'){
+    comments = <div className='centered'><LoadingSpinner/></div>
+  };
+
+  if(status === 'completed' && (loadedComments && loadedComments>0)){
+    comments = <CommentsList comments={loadedComments}/>
+  }
+
+  if(status === 'completed' && (!loadedComments || loadedComments.length === 0)){
+    comments = <p className='centered'>No comments were sdded yet!</p>
+  }
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +52,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && 
+      <NewCommentForm 
+      bikerouteId={params.bikerouteId} 
+      onAddedComment={addedCommentHandler} 
+      />
+      }
+      {comments}
     </section>
   );
 };
--- src/pages/RouteDetail.js
+++ src/pages/RouteDetail.js
@@ -1,29 +1,52 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom';
 
 import Highlight from '../components/bikeroutes/Highlight';
 import Comments from '../components/comments/Comments';
+import useHttp from '../hooks/use-http';
+import { getSingleBikeroute } from '../lib/api';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-const DUMMY_ROUTES = [
-    { id: 'r1', name: 'first' },
-    { id: 'r2', name: 'second' },
-];
+// const DUMMY_ROUTES = [
+//     { id: 'r1', name: 'first' },
+//     { id: 'r2', name: 'second' },
+// ];
 
 
 const RouteDetail = () => {
     const match = useRouteMatch();
-    const params = useParams()
+    const params = useParams();
 
-    const bikeroute = DUMMY_ROUTES.find((bikeroute) => bikeroute.id === params.bikerouteId)
+    const {bikerouteId} = params;
+
+    const {sendRequest, status, data: loadedBikeroute, error} =  useHttp(getSingleBikeroute, true);
+
+    // const bikeroute = DUMMY_ROUTES.find((bikeroute) => bikeroute.id === params.bikerouteId)
     
-    if (!bikeroute) {
+    useEffect(()=> {
+        sendRequest(bikerouteId);
+    },[sendRequest, bikerouteId])
+
+    if(status === 'pending'){
         return (
-            <p>No bike route found</p>
-        )
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
     }
+    
+    if(error){
+        return <p className='centered'>{error}</p>
+    }
+    
+    if(!loadedBikeroute.name) {
+        return <p>No bike route found</p>;        
+    }
+
+
     return (
         <Fragment>
-            <Highlight name={bikeroute.name} />
+            <Highlight name={loadedBikeroute.name} />
             <Route path={match.path} exact>
                 <div className='centered'>
                     <Link className='btn--flat' to={`${match.path}/comments`}>
