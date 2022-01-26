2ca98d7aaf8846cd2e6f495d81ac23e005fe446e
mlabu <labucki.michal@gmail.com>
2022-01-26 12:42:50 +0100

    optimize code by using React.lazy for pages

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
-import { useContext } from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
+import React, { useContext, Suspense } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+import AuthPage from "./pages/AuthPage";
+import UserProfile from "./components/profile/UserProfile";
+import AuthContext from "./store/auth-context";
 
 
-import AllRoutes from './pages/AllRoutes';
-import RouteDetail from './pages/RouteDetail';
-import NewRoute from './pages/NewRoute';
-
-import Layout from './components/layout/Layout';
-// import NotFound from './pages/NoFound';
-import AuthPage from './pages/AuthPage';
-import UserProfile from './components/profile/UserProfile';
-import AuthContext from './store/auth-context';
+const NewRoute = React.lazy(() => import("./pages/NewRoute"));
+const RouteDetail = React.lazy(()=> import("./pages/RouteDetail"));
+const AllRoutes = React.lazy(()=> import('./pages/AllRoutes'));
 
 function App() {
   const authCtx = useContext(AuthContext);
 
-
   return (
     <Layout>
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/bikeroutes' />
-      </Route>
-      <Route path='/bikeroutes' exact>
-       <AllRoutes />        
-      </Route>
-      <Route path='/bikeroutes/:bikerouteId'>
-        <RouteDetail />
-      </Route>
-      <Route path='/new-bikeroute'>
-      {authCtx.isLoggedIn && <NewRoute />}
-      {!authCtx.isLoggedIn && <Redirect to='/auth'/>}
-      </Route>
-      {!authCtx.isLoggedIn && (<Route path='/auth' exact>
-        <AuthPage/>
-      </Route> )}
-      <Route path='/profile'>
-      { authCtx.isLoggedIn && <UserProfile />}
-      {!authCtx.isLoggedIn && <Redirect to='/auth' />}
-      </Route>
-      <Route path='*'>
-        <Redirect to='/' />
-      </Route>
-    </Switch>
+      <Suspense fallback={
+        <div className="centered">
+          <LoadingSpinner/>
+        </div>
+      }>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/bikeroutes" />
+          </Route>
+          <Route path="/bikeroutes" exact>
+            <AllRoutes />
+          </Route>
+          <Route path="/bikeroutes/:bikerouteId">
+            <RouteDetail />
+          </Route>
+          <Route path="/new-bikeroute">
+            {authCtx.isLoggedIn && <NewRoute />}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>
+          {!authCtx.isLoggedIn && (
+            <Route path="/auth" exact>
+              <AuthPage />
+            </Route>
+          )}
+          <Route path="/profile">
+            {authCtx.isLoggedIn && <UserProfile />}
+            {!authCtx.isLoggedIn && <Redirect to="/auth" />}
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
